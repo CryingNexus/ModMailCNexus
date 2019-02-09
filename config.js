ffb250231314f6bd3eb1986291af2f7f46a8ebc1
CryingNexus <47480769+CryingNexus@users.noreply.github.com>
2019-02-09 23:01:58 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+
+  "token": "NTQzODE1MDIyMzI1NzkyNzc4.D0CDXA.EQXvP3jr0MGnwYf202P0gDM_Log",
+  "mailGuildId": "401883208511389716",
+  "mainGuildId": "543807212607242260",
+  "logChannelId": "542772879880880128",
+  "newThreadCategoryId":"542772805003902976",
+
+  "status": "Message me for help!",
+  "responseMessage": "Thank you for your message! Our mod team will reply to you here as soon as possible."
+}
